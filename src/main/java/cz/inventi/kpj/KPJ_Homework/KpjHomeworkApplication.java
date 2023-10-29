371e39bfb81c738a75c2b5b1f951ca171ab6a0d1
Vojtech Leskovsky <Leskivojt@gmail.com>
2023-10-29 16:56:11 +0100

    create empty project

---
+++ src/main/java/cz/inventi/kpj/KPJ_Homework/KpjHomeworkApplication.java
@@ -0,0 +1,14 @@
+package cz.inventi.kpj.KPJ_Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpjHomeworkApplication
+{
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpjHomeworkApplication.class, args);
+	}
+
+}
