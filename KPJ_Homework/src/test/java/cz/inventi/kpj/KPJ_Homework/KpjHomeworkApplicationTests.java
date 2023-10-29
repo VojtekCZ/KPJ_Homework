1af9bb7744df8a86bfef17cc5047a5abb56a98aa
Vojtěch Leskovský <105431938+VojtekCZ@users.noreply.github.com>
2023-10-29 16:59:32 +0100

    Add files via upload

---
+++ KPJ_Homework/src/test/java/cz/inventi/kpj/KPJ_Homework/KpjHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.inventi.kpj.KPJ_Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpjHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
