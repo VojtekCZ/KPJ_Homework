64680d8fe214c08299dab29e6ddf51470da47254
VojtekCZ <leskivojt@gmail.com>
2023-10-30 16:47:29 +0100

    update classes

---
+++ src/main/java/cz/inventi/kpj/KPJ_Homework/database/MessageEntity.java
@@ -0,0 +1,20 @@
+package cz.inventi.kpj.KPJ_Homework.database;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Data
+@Getter
+@Setter
+public class MessageEntity {
+
+    @Id
+    private String serviceName;
+    private int port;
+
+
+}
